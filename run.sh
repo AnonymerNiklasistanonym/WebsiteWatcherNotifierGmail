d1bf1a5bd3160260e06583134fc2eed055f6068e
Niklas <AnonymerNiklasistanonym@users.noreply.github.com>
2020-05-26 16:41:43 +0200

    Update run.sh
    Clarify if the use of the run.sh script is necessary.

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script is optional and only exists to run it isolated from other Python programs.
+# To run it without this script simply run 'pip install -r requirements.txt' and then
+# 'python3 -m main'.
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PYTHON_VENV_DIR=$SCRIPT_DIR/venv_website_watcher
 PYTHON_VENV_REQUIREMENTS_FILE=$SCRIPT_DIR/requirements.txt
@@ -20,5 +24,5 @@ else
     source "$PYTHON_VENV_DIR/bin/activate"
 fi
 
-# Run tests
+# Run script in virtual environment
 python3 -m main
