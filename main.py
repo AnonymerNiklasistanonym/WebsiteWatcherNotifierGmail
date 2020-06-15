3d91f2be27973dc80f051a05f751ee4bf04ad0d2
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-06-15 17:14:08 +0200

    Enable whitelisting of tags to drop and recursive removal
    of attributes and tags

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 import requests
 import base64
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, ResultSet
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -18,8 +18,9 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 
 def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict,
-                         tags_to_drop: Optional[List[str]] = None,
-                         attributes_to_drop: Optional[List[str]] = None) -> str:
+                         tags_to_drop: Optional[List[Tuple[str, List[Tuple[str, List[str]]]]]] = None,
+                         attributes_to_drop: Optional[List[str]] = None,
+                         fix_links_with_base_url: Optional[str] = None) -> str:
     """Scrape web page for a certain part.
 
     Args:
@@ -27,15 +28,17 @@ def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict
         element_tag: HTML element tag that should be looked for.
         element_tag_specifier: Specifier for certain id ({id: '...'}) or class ({class: '...'}).
         tags_to_drop: Optional list of HTML tags that should be dropped from the web page output.
+                      Optionally a whitelist for certain attributes with optional a whitelist of values can be added)
         attributes_to_drop: Optional list of HTML tag attributes that should be dropped from the web page output.
+        fix_links_with_base_url: Optional base url correction for links.
 
     Returns:
         A string that contains a certain part of the website while missing unnecessary style or other information.
     """
     if attributes_to_drop is None:
-        attributes_to_drop = ['class', 'id', 'name', 'style', 'align']
+        attributes_to_drop: List[str] = ['class', 'id', 'name', 'style', 'align']
     if tags_to_drop is None:
-        tags_to_drop = ['script', 'form']
+        tags_to_drop: List[Tuple[str, List[Tuple[str, List[str]]]]] = [('script', []), ('form', [])]
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -45,16 +48,50 @@ def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict
         raise Exception("{}s (with {}) are either not found or too many ({})!".format(element_tag,
                                                                                       element_tag_specifier,
                                                                                       len(found_elements)))
-    element = found_elements[0]
+    element: ResultSet = found_elements[0]
+
+    # with open('original.html', 'w') as token:
+    #     token.write(str(element))
+
     # Remove specified script tags
-    for tag_to_drop in tags_to_drop:
-        for s in element.select(tag_to_drop):
-            s.decompose()
+    helper_remove_tags(element, tags_to_drop)
     # Remove specified attributes from tags
+    helper_remove_attributes(element, attributes_to_drop, fix_links_with_base_url)
+    return str(element)
+
+
+def helper_remove_tags(element: ResultSet, tags_to_drop: Optional[List[Tuple[str, List[Tuple[str, List[str]]]]]]):
+    for tag_to_drop in tags_to_drop:
+        for s in element.select(tag_to_drop[0]):
+            print("s", s)
+            decomposed = False
+            if len(tag_to_drop[1]) > 0:
+                for attribute in tag_to_drop[1]:
+                    if not s.has_attr(attribute[0]):
+                        decomposed = True
+                        s.decompose()
+                    elif len(attribute[1]) == 0 or s[attribute[0]] not in attribute[1]:
+                        decomposed = True
+                        s.decompose()
+            else:
+                decomposed = True
+                s.decompose()
+            print("len(s.select(tag_to_drop[0])) != 0", s)
+            if not decomposed and len(s.select(tag_to_drop[0])) != 0:
+                helper_remove_tags(s, tags_to_drop)
+
+
+def helper_remove_attributes(element: ResultSet, attributes_to_drop: Optional[List[str]],
+                             fix_links_with_base_url: Optional[str]):
     for tag in element():
         for attribute in attributes_to_drop:
-            del tag[attribute]
-    return str(element)
+            if tag.has_attr(attribute):
+                del tag[attribute]
+        if fix_links_with_base_url is not None:
+            if tag.has_attr("href") and not tag["href"].startswith(fix_links_with_base_url):
+                tag["href"] = fix_links_with_base_url + tag["href"]
+        if len(tag()) != 0:
+            helper_remove_attributes(tag, attributes_to_drop, fix_links_with_base_url)
 
 
 def create_gmail_email(sender: str, to: str, subject: str, message_text: str):
@@ -152,7 +189,13 @@ if __name__ == '__main__':
     # Download credentials.json from https://developers.google.com/gmail/api/quickstart/python
     web_page_content = get_web_page_content(url="https://www.nubert.de/stative-halter/38/",
                                             element_tag="div",
-                                            element_tag_specifier={"class": "main-column"})
+                                            element_tag_specifier={"class": "main-column"},
+                                            fix_links_with_base_url="https://www.nubert.de",
+                                            tags_to_drop=[
+                                                ('script', []), ('form', [
+                                                    ("action", ["/basket_add/"])
+                                                ]), ('select', [])
+                                            ])
 
     detected_change = detect_change(file_name='content.html', new_content=web_page_content)
     if detected_change is not None:
