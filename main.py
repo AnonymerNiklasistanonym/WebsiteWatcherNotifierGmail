f4fee5dca55336a7cf4e8e3715a650a00ef7fff2
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2020-11-02 11:08:38 +0100

    Format file using black and make log output readable
    This helps when reading the logs to know which recipient and which
    configuration actually failed

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from dataclasses import dataclass
 
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 Attribute = str
 AttributeValueWhitelist = List[str]
@@ -39,10 +39,14 @@ class Configuration:
     name: str
 
 
-def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict,
-                         tags_to_drop: Optional[List[TagInfo]] = None,
-                         attributes_to_drop: Optional[List[str]] = None,
-                         fix_links_with_base_url: Optional[str] = None) -> str:
+def get_web_page_content(
+    url: str,
+    element_tag: str,
+    element_tag_specifier: dict,
+    tags_to_drop: Optional[List[TagInfo]] = None,
+    attributes_to_drop: Optional[List[str]] = None,
+    fix_links_with_base_url: Optional[str] = None,
+) -> str:
     """Scrape web page for a certain part.
 
     Args:
@@ -58,18 +62,20 @@ def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict
         A string that contains a certain part of the website while missing unnecessary style or other information.
     """
     if attributes_to_drop is None:
-        attributes_to_drop = ['class', 'id', 'name', 'style', 'align']
+        attributes_to_drop = ["class", "id", "name", "style", "align"]
     if tags_to_drop is None:
-        tags_to_drop = [('script', []), ('form', [])]
+        tags_to_drop = [("script", []), ("form", [])]
 
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     # Extract content that should be checked
     found_elements = soup.findAll(element_tag, element_tag_specifier)
     if len(found_elements) != 1:
-        raise Exception("{}s (with {}) are either not found or too many ({})!".format(element_tag,
-                                                                                      element_tag_specifier,
-                                                                                      len(found_elements)))
+        raise Exception(
+            "{}s (with {}) are either not found or too many ({})!".format(
+                element_tag, element_tag_specifier, len(found_elements)
+            )
+        )
     element = found_elements[0]
 
     # with open('original.html', 'w') as token:
@@ -85,7 +91,9 @@ def get_web_page_content(url: str, element_tag: str, element_tag_specifier: dict
     return str(element.prettify())
 
 
-def helper_remove_tags(element: ResultSet, tags_to_drop: Optional[List[TagInfo]] = None):
+def helper_remove_tags(
+    element: ResultSet, tags_to_drop: Optional[List[TagInfo]] = None
+):
     for tag_to_drop in tags_to_drop:
         for s in element.select(tag_to_drop[0]):
             decomposed = False
@@ -104,14 +112,19 @@ def helper_remove_tags(element: ResultSet, tags_to_drop: Optional[List[TagInfo]]
                 helper_remove_tags(s, tags_to_drop)
 
 
-def helper_remove_attributes(element: ResultSet, attributes_to_drop: Optional[List[str]],
-                             fix_links_with_base_url: Optional[str]):
+def helper_remove_attributes(
+    element: ResultSet,
+    attributes_to_drop: Optional[List[str]],
+    fix_links_with_base_url: Optional[str],
+):
     for tag in element():
         for attribute in attributes_to_drop:
             if tag.has_attr(attribute):
                 del tag[attribute]
         if fix_links_with_base_url is not None:
-            if tag.has_attr("href") and not tag["href"].startswith(fix_links_with_base_url):
+            if tag.has_attr("href") and not tag["href"].startswith(
+                fix_links_with_base_url
+            ):
                 tag["href"] = fix_links_with_base_url + tag["href"]
         if len(tag()) != 0:
             helper_remove_attributes(tag, attributes_to_drop, fix_links_with_base_url)
@@ -129,11 +142,11 @@ def create_gmail_email(sender: str, to: str, subject: str, message_text: str):
     Returns:
         An object containing a base64url encoded email object.
     """
-    message = MIMEText(message_text, 'html')
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+    message = MIMEText(message_text, "html")
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+    return {"raw": base64.urlsafe_b64encode(message.as_string().encode()).decode()}
 
 
 def send_gmail_email(service, user_id: str, message):
@@ -148,12 +161,12 @@ def send_gmail_email(service, user_id: str, message):
         Sent email information if successful.
     """
     try:
-        sent_message_info = service.users().messages().send(userId=user_id, body=message).execute()
-        print('Message Id: %s' % sent_message_info['id'])
+        sent_message_info = (
+            service.users().messages().send(userId=user_id, body=message).execute()
+        )
         return sent_message_info
     except Exception as e:
-        print('An error occurred: %s' % e)
-        return None
+        raise e
 
 
 def get_gmail_service():
@@ -162,22 +175,21 @@ def get_gmail_service():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
             credentials = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             credentials.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             credentials = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open("token.pickle", "wb") as token:
             pickle.dump(credentials, token)
 
-    return build('gmail', 'v1', credentials=credentials)
+    return build("gmail", "v1", credentials=credentials)
 
 
 def detect_change(file_name: str, new_content: str) -> Optional[str]:
@@ -193,7 +205,7 @@ def detect_change(file_name: str, new_content: str) -> Optional[str]:
     difference_was_detected = False
     change = None
     if os.path.isfile(file_name):
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             old_content = file.read()
             if old_content != new_content:
                 difference_was_detected = True
@@ -203,57 +215,75 @@ def detect_change(file_name: str, new_content: str) -> Optional[str]:
         change = new_content
 
     if difference_was_detected:
-        with open(file_name, 'w') as file:
+        with open(file_name, "w") as file:
             file.write(new_content)
     return change
 
 
 def load_configuration() -> List[Configuration]:
     configurations = []
-    with open('configuration.json', 'r') as file:
-        data = json.loads(file.read().replace('\n', ''))
-        for job in data['jobs']:
+    with open("configuration.json", "r") as file:
+        data = json.loads(file.read().replace("\n", ""))
+        for job in data["jobs"]:
             tags_to_drop = []
-            for tag_to_drop in job['tags_to_drop']:
+            for tag_to_drop in job["tags_to_drop"]:
                 attribute_whitelist = []
                 for attribute_whitelist_element in tag_to_drop["attribute_whitelist"]:
-                    attribute_whitelist.append((
-                        attribute_whitelist_element['attribute'],
-                        attribute_whitelist_element['attribute_value_whitelist']
-                    ))
-                tags_to_drop.append((
-                    tag_to_drop['tag_to_drop'],
-                    attribute_whitelist
-                ))
-            configurations.append(Configuration(
-                element_tag=job['element_tag'],
-                element_tag_specifier=job['element_tag_specifier'],
-                fix_links_with_base_url=job['fix_links_with_base_url'],
-                tags_to_drop=tags_to_drop,
-                url=job['url'],
-                recipients=job['recipients'],
-                sender=data['sender'],
-                title=job['title'],
-                name=job['name'],
-            ))
+                    attribute_whitelist.append(
+                        (
+                            attribute_whitelist_element["attribute"],
+                            attribute_whitelist_element["attribute_value_whitelist"],
+                        )
+                    )
+                tags_to_drop.append((tag_to_drop["tag_to_drop"], attribute_whitelist))
+            configurations.append(
+                Configuration(
+                    element_tag=job["element_tag"],
+                    element_tag_specifier=job["element_tag_specifier"],
+                    fix_links_with_base_url=job["fix_links_with_base_url"],
+                    tags_to_drop=tags_to_drop,
+                    url=job["url"],
+                    recipients=job["recipients"],
+                    sender=data["sender"],
+                    title=job["title"],
+                    name=job["name"],
+                )
+            )
     return configurations
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Download credentials.json from https://developers.google.com/gmail/api/quickstart/python
     for configuration in load_configuration():
-        web_page_content = get_web_page_content(url=configuration.url,
-                                                element_tag=configuration.element_tag,
-                                                element_tag_specifier=configuration.element_tag_specifier,
-                                                fix_links_with_base_url=configuration.fix_links_with_base_url,
-                                                tags_to_drop=configuration.tags_to_drop)
+        web_page_content = get_web_page_content(
+            url=configuration.url,
+            element_tag=configuration.element_tag,
+            element_tag_specifier=configuration.element_tag_specifier,
+            fix_links_with_base_url=configuration.fix_links_with_base_url,
+            tags_to_drop=configuration.tags_to_drop,
+        )
 
-        detected_change = detect_change(file_name=f"content_{configuration.name}.html", new_content=web_page_content)
+        detected_change = detect_change(
+            file_name=f"content_{configuration.name}.html", new_content=web_page_content
+        )
         if detected_change is not None:
+            print(f"change detected: {configuration.name}")
             gmail_service = get_gmail_service()
             for recipient in configuration.recipients:
-                email = create_gmail_email(sender=configuration.sender, to=recipient,
-                                           subject=configuration.title, message_text=detected_change)
-                send_gmail_email(gmail_service, 'me', email)
+                email = create_gmail_email(
+                    sender=configuration.sender,
+                    to=recipient,
+                    subject=configuration.title,
+                    message_text=detected_change,
+                )
+                try:
+                    sent_message_info = send_gmail_email(gmail_service, "me", email)
+                    print(
+                        f"> sent email: {configuration.name} ({sent_message_info['id']}|{recipient})"
+                    )
+                except Exception as e:
+                    print(
+                        f"> an error occurred during sending the email: {configuration.name} ({recipient})\n{str(e)}"
+                    )
         else:
-            print("no change")
+            print(f"no change: {configuration.name}")
